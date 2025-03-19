762987839ff2ec66a0c69f114ed051998009f18d
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2025-03-19 14:33:37 -0700

    Mapeo entidad consumible con typeorm

--- server/entitys/Consumible.js
+++ server/entitys/Consumible.js
@@ -1,5 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
 class Consumible {
-    constructor(id, nombre, descripcion, cantidad){
+    @PrimaryGeneratedColumn()
+    id;
+
+    @Column({ nullable: false })
+    nombre;
+
+    @Column()
+    descripcion;
+
+    @Column()
+    cantidad;
+
+    constructor(id, nombre, descripcion, cantidad) {
         this.id = id;
         this.nombre = nombre;
         this.descripcion = descripcion;
